59f92bb532ca4bbb074823b3dc537b36cb3df51d
TamadaD <meister.tamada@gmail.com>
2018-12-22 14:22:03 +0900

    ???

---
+++ Tamada.py
@@ -0,0 +1,54 @@
+# -*- coding : utf-8 -:-
+#import
+import serial
+import time
+import csv
+
+#関数定義
+val_size = 4
+values = [0 for x in range(val_size)]
+isValids = [False for x in range(val_size)]
+ser = serial.Serial('/Device/USBPDO-4',9600,timeout = 0.1)
+path = 'test.csv'
+fieldname = ['tp','rh','ir','co2ppm']
+
+with open(path, 'w') as f:
+    writer = csv.DictWriter(f, fieldnames=fieldname)
+    writer.writeheader()
+
+f.close
+
+#処理
+while True:
+#ビットシフト
+    headByte = ser.read()
+    head = int.from_bytes(headByte, 'big')
+    if head == 128:
+        isValids = [False for x in range(val_size)]
+
+    for i in range(val_size):
+        if head == 128+i:
+            highByte = ser.read()
+            lowByte = ser.read()
+            high = int.from_bytes(highByte, 'big')
+            low = int.from_bytes(lowByte, 'big')
+            values[i] = (high<<7) + low
+            if 0 <= values[i] and values[i] <= 1023:
+                isValids[i] = True
+
+#リスト内の要素を整理、/10して元のけたに戻す
+    if all(i == True for i in isValids):
+        tp = values[0]/10
+        rh = values[1]/10
+        ir = values[2]/10
+        co2ppm = values[3]/10
+
+        with open(path, 'a') as f:
+            writer = csv.DictWriter(f, fieldnames=fieldname)
+            writer.writerow({'tp':tp,'rh':rh,'ir':ir,'co2ppm':co2ppm})
+
+        f.close
+        #出力
+        print(str(tp) + '℃' + str(rh)+ '%' + str(ir) + str(co2ppm) + '%')
+
+time.sleep(1)
---
+++ Tamada1/Tamada1.ino
@@ -0,0 +1,83 @@
+/*----------------------------------------------------------------------
+　　全部のセンサー動かして，シリアル通信
+---------------------------------------------------------------------- */
+#include <Wire.h>
+#include <MHZ19_uart.h>
+
+#define DEV_ADR 0x5c  // AM2320スレーブアドレス
+#define LED 13  //確認用LED
+
+uint8_t data[8];
+const int rx_pin = 4;  //Serial rx pin no
+const int tx_pin = 5; //Serial tx pin no
+int pin2 = 0;
+int all = 0;
+int val = 4;
+
+MHZ19_uart mhz19;
+
+void setup() {
+	Wire.begin();
+	mhz19.begin(rx_pin, tx_pin);
+	mhz19.setAutoCalibration(false);
+	Serial.begin(9600);
+	Serial.print("MH-Z19 now warming up...  status:");
+	Serial.println(mhz19.getStatus());
+	delay(500);
+}
+
+void loop() {
+	int co2ppm = mhz19.getPPM();
+	int temp = mhz19.getTemperature();
+	int date[1000];
+
+	// AM2320センサーのウェイクアップ
+	Wire.beginTransmission(DEV_ADR);
+	Wire.endTransmission();
+
+	// 湿度・温度データ取得要求
+	Wire.beginTransmission(DEV_ADR);
+	Wire.write(0x03);         // レジスタ読み取りコマンド
+	Wire.write(0x00);         // 読み取り開始レジスタ番号
+	Wire.write(0x04);         // 読み取りデータ数    
+	Wire.endTransmission();
+
+	// データの取得
+	Wire.requestFrom(DEV_ADR, 8);
+
+	for (int del = 0; del < 1000; del++)
+	{
+		pin2 = analogRead(A0);
+		// Serial.println(pin2);   
+		date[del] = pin2;
+		delay(10);
+
+		all = all + date[del];
+	}
+
+	int ir = all / 1000;
+
+	if (Wire.available() >= 8) {
+		for (uint8_t i = 0; i < 8; i++)
+		{
+			data[i] = Wire.read();
+		}
+
+		int rh = ((int)data[2] * 256 + data[3]) * 10; // 湿度
+		int tp = ((int)data[4] * 256 + data[5]) * 10; // 温度
+
+		int valu[val] = { tp,rh,ir,co2ppm };    //リストの制作
+
+		for (int j = 0; j < val; j++)
+		{
+			int high = (valu[j] >> 7) & 127;
+			int low = valu[j] & 127;
+
+			Serial.write(128 + j);　　　　　　　//raspberry pi 用のデータ送信
+			Serial.write(high);
+			Serial.write(low);
+		}
+
+	}
+	delay(1000);
+}
