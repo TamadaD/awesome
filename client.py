c2d797cbaef89be7b54381c16d935deda6622363
Umisyo <kusunokisouta@gmail.com>
2019-02-04 01:16:05 +0900

    'test'

--- client.py
+++ client.py
@@ -13,12 +13,10 @@ print ('Recieved"%s"' % result)
 
 def main():
     tp, rh, ir, co2ppm = 1, 2, 3, 5
-
     ws.send (str(tp) + ',' + str(rh) + ',' + str(ir) + ',' + str(co2ppm))
-
-    ws.close ()
-
 
 while True:
     main()
+    ws.close
     time.sleep(1)
+
