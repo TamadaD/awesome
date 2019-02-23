12c65ba4464acca9a7626aafa62d1fb5c6728812
Umisyo <kusunokisouta@gmail.com>
2019-02-24 02:15:41 +0900

    ino

--- Tamada_Censor/Tamada_Censor.ino
+++ Tamada_Censor/Tamada_Censor.ino
@@ -26,11 +26,11 @@ long time_low = low_level_end - low_level_start;
 long co2ppm = 5000 * (time_high - 0.002) / (time_high + time_low - 0.004);
 
 void setup() {
-  
+
     Wire.begin();
     Serial.begin(9600);
     pinMode(PWM_PIN, INPUT);
- 
+
 }
 
 void loop() {
@@ -40,7 +40,7 @@ long high_level_start = 0L;
 long high_level_end = 0L;
 long low_level_start = 0L;
 long low_level_end = 0L;
- 
+
   long cycle_start_time = millis();
   int pin_in = digitalRead(PWM_PIN);
 
@@ -65,10 +65,10 @@ long low_level_end = 0L;
     }
   }
 
-   
-      
+
+
         uint8_t data[8];
-  
+
   // AM2320センサーのウェイクアップ
   Wire.beginTransmission(DEV_ADR);
   Wire.endTransmission();
@@ -77,22 +77,22 @@ long low_level_end = 0L;
   Wire.beginTransmission(DEV_ADR);
   Wire.write(0x03);         // レジスタ読み取りコマンド
   Wire.write(0x00);         // 読み取り開始レジスタ番号
-  Wire.write(0x04);         // 読み取りデータ数    
+  Wire.write(0x04);         // 読み取りデータ数
   Wire.endTransmission();
 
   // データの取得
-  Wire.requestFrom(DEV_ADR,8); 
+  Wire.requestFrom(DEV_ADR,8);
   if (Wire.available() >= 8) {
     for (uint8_t i=0; i<8; i++) {
       data[i] = Wire.read();
     }
-    
+
     float rh = ((float)(data[2]*256+data[3]))/10; // 湿度
     float tp = ((float)(data[4]*256+data[5]))/10; // 温度
-    
+
     // 湿度・温度の表示
   }
-      
+
 
 
 int ir = analogRead(A0) ;
@@ -101,9 +101,17 @@ int ir = analogRead(A0) ;
     float tp = ((float)(data[4]*256+data[5]))/10; // 温度
 
 long co2ppm = 5000 * (time_high - 0.002) / (time_high + time_low - 0.004);
-Serial.print("PPM = " + String(co2ppm));
-Serial.println(ir);
-Serial.print(tp);
-Serial.println(rh);
-}
+
+int valu[val] = {tp,rh,ir,co2ppm};    //リストの制作
     
+        for(int j=0;j<val;j++)
+        {
+        int high = (valu[j] >> 7)&127;
+        int low  = valu[j]&127;
+        
+        Serial.write(128+j);//raspberry pi 用のデータ送信
+        Serial.write(high);
+        Serial.write(low);  
+          }
+
+}
